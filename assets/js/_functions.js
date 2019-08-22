55f3056b372a27d12a3017a7ac93bc362a74fa63
jannickvossh <jannickhaunstrup@gmail.com>
2019-08-22 12:12:26 +0200

    connect category selector to post categories

--- assets/js/_functions.js
+++ assets/js/_functions.js
@@ -3,6 +3,9 @@
 const categorySelector = document.getElementById('category-selector');
 const categorySwitch = document.getElementById('category-switch');
 const category = document.querySelectorAll('.category');
+const postCategories = document.querySelectorAll('.post-grid');
+const personalPosts = postCategories[0];
+const clientsPosts = postCategories[1];
 
 
 categorySelector.addEventListener('click', () => {
@@ -10,9 +13,13 @@ categorySelector.addEventListener('click', () => {
 		category[0].classList.remove('active');
 		category[1].classList.add('active');
 		categorySwitch.classList.add('moved');
+		personalPosts.style.display = "none";
+		clientsPosts.style.display = "flex";
 	} else {
 		category[1].classList.remove('active');
 		category[0].classList.add('active');
 		categorySwitch.classList.remove('moved');
+		clientsPosts.style.display = "none";
+		personalPosts.style.display = "flex";
 	}
 });
